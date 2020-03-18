15fe67ceb85a4ca0aba48185844e11980c73b3f6
Gonçalo Neves <neves.goncaloscp9@gmail.com>
2020-03-18 13:01:57 +0000

    Add ContainerReusablePoolTest

--- tests/com/es2/designpatterns/cargo/MedicamentoTest.java
+++ tests/com/es2/designpatterns/cargo/MedicamentoTest.java
@@ -1,28 +1,28 @@
 package com.es2.designpatterns.cargo;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import com.es2.designpatterns.cargo.Medicamento;
+import org.junit.jupiter.api.Test;
 
 class MedicamentoTest {
 
     @Test
-    void getMedicineValue() {
+    void testCreateMedicamento() {
+
+        Medicamento medicamento1 = new Medicamento();
+        medicamento1.setName("Aspirina");
+        medicamento1.setSize(1);
+        medicamento1.setQuantity(10);
+        medicamento1.setUnitValue(5);
     }
 
     @Test
-    void setUnitValue() {
-    }
+    void testgetMedicineValue() {
 
-    @Test
-    void setQuantity() {
-    }
-
-    @Test
-    void getTransporteItems() {
-    }
-
-    @Test
-    void getTransporteTotalPrice() {
+        Medicamento medicamento1 = new Medicamento();
+        medicamento1.setName("Aspirina");
+        medicamento1.setSize(1);
+        medicamento1.setQuantity(10);
+        medicamento1.setUnitValue(5);
     }
 }
